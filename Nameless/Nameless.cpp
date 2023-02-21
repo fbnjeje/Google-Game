1d9fb263aac0619450848ee3c20fae3dc066d978
Neonx <fabiangomes2015@gmail.com>
2023-02-20 21:46:53 -0500

    Condicion para los limites del mapa

--- Nameless/Nameless.cpp
+++ Nameless/Nameless.cpp
@@ -13,10 +13,25 @@ const int WIDTH = 800, HEIGHT = 600;
 //dimensiones de los pixeles
 const int dw = 8, dh = 16;
 
-
+//convertir datos a int
 void concertir(int p[2], float x, float y) {
 	p[0] = int(x) / dw;
 	p[1] = int(y) / dh;
+}
+
+void concertir(int p[2], int x, int y) {
+	p[0] = x / dw;
+	p[1] = y/ dh;
+}
+
+
+//Funcion dibujar
+
+void drawpoint(char plano[HEIGHT / dh][WIDTH / dw], int A, int B, char c)
+{
+	B = HEIGHT / dh - 1 - B;
+	if (A < 0 || B < 0 || A >= WIDTH / dw || B >= HEIGHT / dh) return;
+	plano[B][A] = c;
 }
 
 main() {
